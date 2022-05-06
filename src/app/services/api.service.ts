8578c85bb0590e48ecde9262036de8473c90a655
osas0007 <2021263@student.cct.ie>
2022-05-06 11:55:28 +0000

    clone mikhail

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ApiService {
+
+    constructor(private http: HttpClient) { }
+
+    getEpisodes() {
+        return this.http.get('https://8100-mikhailcct-mobdevca3-4r1lzru6qyb.ws-eu44.gitpod.io/assets/episodes.json')
+    }
+
+    getEpisode(id) {
+        return this.http.get(`https://8100-mikhailcct-mobdevca3-4r1lzru6qyb.ws-eu44.gitpod.io/assets/episode.json`);
+    }
+
+    getCharacters() {
+        return this.http.get('https://8100-mikhailcct-mobdevca3-4r1lzru6qyb.ws-eu44.gitpod.io/assets/characters.json')
+    }
+
+    getCharacter(id) {
+        return this.http.get(`https://www.breakingbadapi.com/api/characters/${id}`);
+    }
+
+    getQuotes() {
+        return this.http.get(`https://www.breakingbadapi.com/api/quotes`)
+    }
+
+    getQuote(id) {
+        return this.http.get(`https://www.breakingbadapi.com/api/quotes/${id}`);
+    }
+
+    getDeaths() {
+        return this.http.get(`https://www.breakingbadapi.com/api/death-count?name=Gustavo+Fring`)
+    }
+
+}
